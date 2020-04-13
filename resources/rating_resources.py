2df717d19140d7a8cee1de132c9ae99f76350e30
mariialevytska <mariia.levytska99@gmail.com>
2020-04-13 13:40:31 +0300

    user ratings

--- resources/rating_resources.py
+++ resources/rating_resources.py
@@ -8,21 +8,22 @@ from models.user_levels import UserLevels
 
 class RatingResources(Resource):
     def get(self):
-        payload = request.get_json(force=True)
-        # user_name = payload.get('user')
-        # user = User.query.filter(User.username == user_name).first()
 
         user_ratings = db.session.query(UserLevels.user_id, func.sum(UserLevels.score).label("total_score"))\
             .group_by(UserLevels.user_id).order_by("total_score").all()
 
-        #user_ratings = db.session.execute(query)
         result = []
+        position = 1
         for user_rating in user_ratings:
+            user = User.query.filter(User.user_id == user_rating.user_id).first()
             result.append(
                 {
                     "score": str(user_rating.total_score),
-                    "user": str(user_rating.user_id)
+                    "username": user.username,
+                    "email": user.email,
+                    "position": position
                 }
             )
+            position += 1
 
         return result
