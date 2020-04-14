385438b958b5e8ca7926983139900b3aeb1d28f7
mariialevytska <mariia.levytska99@gmail.com>
2020-04-14 10:23:09 +0300

    fixed rating page

--- resources/rating_resources.py
+++ resources/rating_resources.py
@@ -8,16 +8,19 @@ from models.user_levels import UserLevels
 
 class RatingResources(Resource):
     def get(self):
-        users = db.session.query(User.username, User.email, func.sum(UserLevels.score).label("total_score")\
-                                 ).join(UserLevels, isouter=True).group_by(User.user_id).\
+        users = db.session.query(User.username, User.email, func.sum(UserLevels.score).label("total_score") \
+                                 ).join(UserLevels, isouter=True).group_by(User.user_id). \
             order_by(desc("total_score")).all()
 
         result = []
         position = 1
         for user in users:
+            score = 0
+            if (user.total_score != None):
+                score = user.total_score
             result.append(
                 {
-                    "score": str(user.total_score),
+                    "score": str(score),
                     "username": user.username,
                     "email": user.email,
                     "position": position
@@ -26,4 +29,3 @@ class RatingResources(Resource):
             position += 1
 
         return result
-
